043ca556ac064a3ad81cce40026abb60c0b6ddf1
lotsses <lotsses@yandex.ru>
2026-03-01 19:03:05 +0300

    Update file

--- schema/load_data.sql
+++ schema/load_data.sql
@@ -1,17 +1,17 @@
-\copy customers FROM 'C:/Users/d3-l5/Documents/olist-sql-project/data/raw/olist_customers_dataset.csv' CSV HEADER;
+\copy customers FROM 'PATH_TO_YOUR_FILE/olist_customers_dataset.csv' CSV HEADER;
 
-\copy geolocation FROM 'C:/Users/d3-l5/Documents/olist-sql-project/data/raw/olist_geolocation_dataset.csv' CSV HEADER;
+\copy geolocation FROM 'PATH_TO_YOUR_FILE/olist_geolocation_dataset.csv' CSV HEADER;
 
-\copy sellers FROM 'C:/Users/d3-l5/Documents/olist-sql-project/data/raw/olist_sellers_dataset.csv' CSV HEADER;
+\copy sellers FROM 'PATH_TO_YOUR_FILE/olist_sellers_dataset.csv' CSV HEADER;
 
-\copy product_category_translation FROM 'C:/Users/d3-l5/Documents/olist-sql-project/data/raw/product_category_name_translation.csv' CSV HEADER;
+\copy product_category_translation FROM 'PATH_TO_YOUR_FILE/product_category_name_translation.csv' CSV HEADER;
 
-\copy products FROM 'C:/Users/d3-l5/Documents/olist-sql-project/data/raw/olist_products_dataset.csv' CSV HEADER;
+\copy products FROM 'PATH_TO_YOUR_FILE/olist_products_dataset.csv' CSV HEADER;
 
-\copy orders FROM 'C:/Users/d3-l5/Documents/olist-sql-project/data/raw/olist_orders_dataset.csv' CSV HEADER;
+\copy orders FROM 'PATH_TO_YOUR_FILE/olist_orders_dataset.csv' CSV HEADER;
 
-\copy order_items FROM 'C:/Users/d3-l5/Documents/olist-sql-project/data/raw/olist_order_items_dataset.csv' CSV HEADER;
+\copy order_items FROM 'PATH_TO_YOUR_FILE/olist_order_items_dataset.csv' CSV HEADER;
 
-\copy payments FROM 'C:/Users/d3-l5/Documents/olist-sql-project/data/raw/olist_order_payments_dataset.csv' CSV HEADER;
+\copy payments FROM 'PATH_TO_YOUR_FILE/olist_order_payments_dataset.csv' CSV HEADER;
 
-\copy reviews FROM 'C:/Users/d3-l5/Documents/olist-sql-project/data/raw/olist_order_reviews_dataset.csv' CSV HEADER ENCODING 'LATIN1';
+\copy reviews FROM 'PATH_TO_YOUR_FILE/olist_order_reviews_dataset.csv' CSV HEADER ENCODING 'LATIN1';
